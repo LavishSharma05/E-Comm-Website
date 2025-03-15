32c6fda3306e64d83629b5a26ea0e82c7f895847
LavishSharma05 <lavish.crick2005@gmail.com>
2025-03-15 23:25:43 +0530

    Added new files

--- Frontend/src/Components/Navbar/Navbar.jsx
+++ Frontend/src/Components/Navbar/Navbar.jsx
@@ -1,18 +1,30 @@
-import React, { useState } from 'react'
+import React, { useContext, useRef, useState } from 'react'
 import './Navbar.css'
 import logo from '../Assets/logo.png'
 import cart_icon from '../Assets/cart_icon.png'
 import { Link } from 'react-router-dom';
+import { ShopContext } from '../../Context/ShopContext';
+import nav_dropdown_icon from '../Assets/nav_dropdown.png'
 
 function Navbar() {
     const [menu,setmenu]=useState('Shop')
+    const {getTotalCartItems}=useContext(ShopContext)
+    const menuRef=useRef()
+
+    const dropdown_toggle=(e)=>{
+        menuRef.current.classList.toggle('nav-menu-visible')
+        e.target.classList.toggle('open')
+    }
+
     return (
         <div className='navbar'>
             <div className="nav-logo">
                 <img src={logo} alt=""/>
                 <p>SHOPPER</p>
             </div>
-            <ul className='nav-menu'>
+            <img className='nav-dropdown' onClick={dropdown_toggle} src={nav_dropdown_icon} alt=''/>
+
+            <ul ref={menuRef} className='nav-menu'>
                 <li onClick={()=>{setmenu("Shop")}}><Link to='/' style={{textDecoration:'none'}}>Shop</Link>{menu==='Shop'?<hr/>:<></>}</li>
                 <li onClick={()=>{setmenu("Men")}}><Link to='/mens' style={{textDecoration:'none'}}>Men</Link>{menu==='Men'?<hr/>:<></>}</li>
                 <li onClick={()=>{setmenu("Women")}}><Link to='/womens' style={{textDecoration:'none'}}>Women</Link>{menu==='Women'?<hr/>:<></>}</li>
@@ -21,7 +33,7 @@ function Navbar() {
             <div className='nav-login-cart'>
                 <Link to='/login'><button>Login</button></Link>
                 <Link to='/cart'><img src={cart_icon} alt=""/></Link>
-                <div className='nav-cart-count'>0</div>
+                <div className='nav-cart-count'>{getTotalCartItems()}</div>
             </div>
         </div>
 
--- Frontend/src/Pages/Product.jsx
+++ Frontend/src/Pages/Product.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import {ShopContext} from '../Context/ShopContext'
+import {useParams} from 'react-router-dom'
+import Breadcrums from '../Components/BreadCrums/Breadcrums'
+import ProductDisplay from '../Components/ProductDisplay/ProductDisplay'
+import RelatedProduct from '../Components/RelatedProduct/RelatedProduct'
 
 function Product() {
+    const {all_product}=useContext(ShopContext)
+    const {productId}=useParams()
+
+    const product=all_product.find((e)=>e.id===Number(productId))
     return (
-        <></>
+        <div className=''>
+            <Breadcrums product={product}/>
+            <ProductDisplay product={product}/>
+            <RelatedProduct />
+        </div>
     )
 }
 
